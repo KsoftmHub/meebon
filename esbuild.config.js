db520dfab94557652bdd48c45038d4fd3e7913ba
kajalanS <103587022+kajalanS@users.noreply.github.com>
2025-05-08 03:37:07 +0530

    fix: remove JSON assertion from package.json import in esbuild config

--- esbuild.config.js
+++ esbuild.config.js
@@ -1,6 +1,6 @@
 import { build } from 'esbuild';
 import { rm } from 'fs/promises';
-import pkg from "./package.json" assert { type: "json" };
+import pkg from "./package.json";
 
 await rm("dist", { force: true, recursive: true });
 
