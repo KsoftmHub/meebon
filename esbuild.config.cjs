1967611eac8e84bbbb2e3c467e82eee180001bbb
kajalanS <103587022+kajalanS@users.noreply.github.com>
2025-05-08 03:42:53 +0530

    fix: update package.json version to 1.2.4 and refactor esbuild config to use CommonJS format

--- esbuild.config.cjs
+++ esbuild.config.cjs
@@ -1,18 +1,20 @@
-import { build } from 'esbuild';
-import { rm } from 'fs/promises';
+const esbuild = require('esbuild');
+const { rm } = require('fs/promises');
 const pkg = require("./package.json");
 
-await rm("dist", { force: true, recursive: true });
+(async () => {
+  await rm("dist", { force: true, recursive: true });
 
-build({
-  entryPoints: ['index.js'],
-  format: "esm",
-  bundle: true,
-  outfile: 'dist/index.js',
-  platform: 'node',
-  // target: 'node18',
-  sourcemap: true,
-  external: Object.keys(pkg.dependencies),
-}).then(() => {
-  console.log('Build completed successfully');
-}).catch(() => process.exit(1));
+  esbuild.build({
+    entryPoints: ['index.js'],
+    format: "cjs",
+    bundle: true,
+    outfile: 'dist/index.js',
+    platform: 'node',
+    // target: 'node18',
+    sourcemap: true,
+    external: Object.keys(pkg.dependencies),
+  }).then(() => {
+    console.log('Build completed successfully');
+  }).catch(() => process.exit(1));
+})();
